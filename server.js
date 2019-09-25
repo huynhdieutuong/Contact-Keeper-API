09c5fb7f36c359a8ce23d6ac6f8ccf4f0a871823
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-09-25 11:11:47 +0700

    Initial server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const connectDB = require("./config/db");
+
+// Connect Database
+connectDB();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
