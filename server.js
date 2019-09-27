6e684aa4515f8f5bbc2e4b77b5361dc9fa65a10b
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-09-27 17:31:54 +0700

    Added auth check and login via token

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 
 // Port
 const port = process.env.PORT || 5000;
